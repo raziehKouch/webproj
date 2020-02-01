01cbfe98dd2be5759a91c07d7606d909d152976d
Razieh Kouch <razieh.kouchakzadeh24@gmail.com>
2020-02-01 18:33:31 +0330

    home tabs

--- blog/models.py
+++ blog/models.py
@@ -13,9 +13,6 @@ class Chanel(models.Model):
     authors = models.ManyToManyField(User, related_name='%(class)s_authors')
     followers = models.ManyToManyField(User, related_name='%(class)s_followers')
 
-    def __str__(self):
-        return self.title
-
 class post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -23,6 +20,3 @@ class post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     chanel = models.ForeignKey(Chanel, on_delete=models.CASCADE, null=True)
     post_pic = models.ImageField(upload_to='static/blog/image/', null=True, blank=True)
-
-    def __str__(self):
-        return self.title
