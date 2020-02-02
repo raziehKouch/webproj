8980b8e78b832c89220665985715675680895af2
maryam <msaberi1813@gmail.com>
2020-02-02 10:40:39 +0330

    chanel detail

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    chanel = models.ForeignKey(Chanel, on_delete=models.CASCADE, null=True)
+    chanel = models.ForeignKey(Chanel, on_delete=models.CASCADE, null=True, blank= True)
     post_pic = models.ImageField(upload_to='post_picd', null=True, blank=True)
 
 
