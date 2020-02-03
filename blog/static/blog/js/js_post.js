a6a7e7dd1e768fed986e2a06a62ff3dedca6a386
msaberi1813 <msaberi1813@gmail.com>
2020-02-03 23:19:58 +0330

    update

--- blog/static/blog/js/js_post.js
+++ blog/static/blog/js/js_post.js
@@ -8,20 +8,3 @@ function myFunction(id) {
   }
 }
 
-function Copy(){
- var textArea = document.createElement("textarea");
-  textArea.value = text;
-  textArea.style.position="fixed";  //avoid scrolling to bottom
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-
-      var text = document.createElement("textarea");
-                text.innerHTML = window.location.href;
-//                alert(text.innerHTML)
-                Copied = text.createTextRange();
-                Copied.select();
-                Copied.setSelectionRange(0, 99999)
-                Copied.execCommand("Copy");
-                alert("copied",window.location.href )
-}
